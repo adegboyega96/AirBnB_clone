57a3c8ccd2becf2dbbc8e484d3d2b53aaa70f22f
Emmanuel Adetoro <emmanueladetoro72@gmail.com>
2023-05-14 18:09:00 +0100

    First User

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,9 +26,11 @@ class FileStorage:
     def classes(self):
         """Returns a dictionary of valid classes and their references"""
         from models.base_model import BaseModel
+        from models.user import User
 
         classes = {
             "BaseModel": BaseModel,
+            "User": User,
         }
         return classes
 
--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""User class module"""
+"""User class module"""
 from models.base_model import BaseModel
 
 
